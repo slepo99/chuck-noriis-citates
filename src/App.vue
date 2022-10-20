c6944bc387d7cf358c9cf98628c6172a8bd8b1d8
Timur <slepospb@gmail.com>
2022-10-20 17:35:22 +0500

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,170 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="main">
+    <div class="header">
+      <div class="header_items">
+        <img src="@/assets/logo.png" alt="chuk norris little logo" />
+        <p>Chuck Norris</p>
+      </div>
+    </div>
+    <div class="container">
+      <p class="title">Categories</p>
+      <div class="themes">
+        <div v-for="item in data" :key="item.id">
+          <div @click="getCitate(item.id)" class="themes-tag">
+            <p class="themes-text">{{ item.theme }}</p>
+          </div>
+        </div>
+      </div>
+      <Quotes :quotes="quotes" />
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Quotes from "./components/Quotes.vue";
+
+export default {
+  components: { Quotes },
+  data() {
+    return {
+      data: [
+        {
+          theme: "career",
+          citate: "-“Hi, Im Chuck Norris and I start tomorrow. Any questions?”",
+        },
+        {
+          theme: "love",
+          citate:
+            "-“What we have once enjoyed we can never lose. All that we love deeply becomes a part of us.”",
+        },
+        {
+          theme: "celebrity",
+          citate:
+            "-“There is no such thing as fun for the whole family.” —Jerry Seinfeld",
+        },
+        {
+          theme: "fashion",
+          citate:
+            " -“Clothes mean nothing until someone lives in them. Marc Jacobs”",
+        },
+        { theme: "money", citate: "-“Money often costs too much.”" },
+        { theme: "music", citate: "-“Music is a moral law.”" },
+        {
+          theme: "political",
+          citate:
+            "-“Your time is limited, so don't waste it living someone else's life.”",
+        },
+        {
+          theme: "sport",
+          citate:
+            "-“ The only way to prove that you're a good sport is to lose.””",
+        },
+        { theme: "travel", citate: "-“Travel is my therapy.”" },
+      ],
+      quotes: [],
+    };
+  },
+  computed: {
+    addKey() {
+      this.data = this.data.map((item, id) => {
+        let uniqId = parseInt(id);
+        return Object.assign(item, { id: uniqId + 1 });
+      });
+    },
+  },
+
+  methods: {
+    getCitate(item) {
+      if ((this.quotes = [])) {
+        return (this.quotes = this.data.filter(function (i, index) {
+          return index + 1 === item;
+        }));
+      }
+    },
+  },
+  mounted() {
+    this.addKey;
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0 auto;
+  background: #e5e5e5;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.container {
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+.themes {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: space-between;
+  align-content: center;
+  margin-bottom: 150px;
+}
+.themes-tag {
+  background: #ffffff;
+  border: 0.75px solid #1c1c39;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.24);
+  border: none;
+  border-radius: 20px;
+  width: 80px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+.themes-tag:hover {
+  background: #422ed4;
+}
+.themes-text {
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+  color: #1c1c39;
+}
+.themes-text:hover {
+  color: #ffffff;
+}
+.title {
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 50px;
+  line-height: 59px;
+  /* identical to box height */
+  color: #1c1c39;
+  text-align: center;
+  margin-top: 50px;
+}
+.header {
+  position: fixed;
+  width: 100%;
+  height: 35px;
+  background: #422ed4;
+  color: #ffffff;
+}
+.header_items {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 150px;
+  align-items: center;
+  margin-left: 20px;
+}
+.header_items img {
+  width: 35px;
+  height: 35px;
 }
 </style>
---
+++ src/components/Quotes.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="quote-container">
+    <div v-if="this.quotes.length < 1" class="some-class">
+      <p class="quote_text">Select some quote theme</p>
+    </div>
+
+    <div v-for="quote in quotes" :key="quote.id">
+      <p class="quote_text">{{ quote.citate }}</p>
+    </div>
+
+    <div>
+      <img src="@/assets/chuckNorris.png" alt="Chuk Norris" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "quotes",
+  props: {
+    quotes: {
+      type: [Object, Array],
+    },
+  },
+};
+</script>
+
+<style scoped>
+.quote-container {
+  height: 140px;
+  width: 900px;
+  background: #422ed4;
+  box-shadow: 0px 100px 80px rgba(80, 76, 103, 0.07),
+    0px 0px 10.0172px rgba(80, 76, 103, 0.035);
+  border-radius: 25px;
+  margin-bottom: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.quote_text {
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 150%;
+  /* or 30px */
+  width: 800px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+
+  color: #ffffff;
+}
+@media (max-width: 900px) {
+  .quote-container {
+    max-width: 600px;
+  }
+  .quote_text {
+    max-width: 600px;
+  }
+}
+@media (max-width: 600px) {
+  .quote-container {
+    max-width: 300px;
+  }
+  .quote_text {
+    max-width: 300px;
+  }
+}
+</style>
